2c1f8f14f9a660eb923004852a308c3e00c61d2b
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-12-11 15:54:41 +0100

    header fixed

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,13 +3,16 @@ import './Header.scss'
 
 const Header = () => {
   return (
-    <header>
-        <img className='header' src="src/assets/vog-logo.png" alt="logo" />
-        <h1 className='header'>VirisOnGithub</h1>
-        <a className='header' href='#'>Accueil</a>
-        <a className='header' href='#projets'>Projets</a>
-        <a className='header' href='#apropos'>A propos</a>
-    </header>
+    <>
+      <header>
+          <img className='header' src="src/assets/vog-logo.png" alt="logo" />
+          <h1 className='header'>VirisOnGithub</h1>
+          <a className='header' href='#'>Accueil</a>
+          <a className='header' href='#projets'>Projets</a>
+          <a className='header' href='#apropos'>A propos</a>
+      </header>
+      <div className="headercontainer"></div>
+    </>
   )
 }
 
