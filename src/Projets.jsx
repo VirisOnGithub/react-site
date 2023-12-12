53575d9492cc7ccf4155aafa94969b4e4fc85078
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-12-12 21:28:08 +0100

    clean before bootstrap

--- src/Projets.jsx
+++ src/Projets.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Projets.scss'
-import Projet from './Projet.jsx'
 
 const Projets = () => {
   return (
     <div className='projects'>
         <h1>Projets</h1>
         <div className="jeux">
-            <Projet color="#FF0000" img="./src/assets/vog-logo.png" />
+            
         </div>
         <div className="autres">
 
